de5890846f71bb52f40f97f4aa57209d19feeeec
AYA <aya101855@gmail.com>
2024-12-14 16:12:09 +0200

    make changes

--- src/main.js
+++ src/main.js
@@ -3,31 +3,10 @@ import 'bootstrap';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import Activity from './Views/activity.vue';
-import Research from './Views/research.vue';
-import Research_admin from './Views/research_admin.vue';
-import Units from './Views/Units.vue';
-
-import './assets/style.css';
-
-
-// إعداد المسارات (Routes)
-const routes = [
-  { path: '/activity', component: Activity },
-  { path: '/research', component: Research },
-  { path: '/research_admin', component: Research_admin },
-  { path: '/units', component: Units },
-];
-
-// إنشاء الRouter
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+import router from './router';  // استيراد الـ router من الملف الجديد
+import './assets/style/base.css';
 
 const app = createApp(App);
 app.use(router); // إضافة Vue Router للتطبيق
-
 
 app.mount('#app');
