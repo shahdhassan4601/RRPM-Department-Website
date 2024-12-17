1f08a84bac01827ae6c077cebac4cbc7190a9428
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-12-17 04:05:09 +0200

    [ADD]: add the SingleResearch page and add its route

--- rrpm-department/src/router/index.js
+++ rrpm-department/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SingleActivity from "../views/SingleActivity.vue";
+import SingleResearch from "../views/SingleResearch.vue";
 
 const routes = [
   { path: "/activities/:id", name: "SingleActivity", component: SingleActivity, props: true },
+  { path: "/research/:id", name: "SingleResearch", component: SingleResearch, props: (route) => ({ id: route.params.id })},
 ];
 
 const router = createRouter({
