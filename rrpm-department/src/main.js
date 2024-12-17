51bc525c25c6de3959d33d3019f1568fa93421c8
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-12-17 04:36:04 +0200

    [Remove]: remove the part of i18n library

--- rrpm-department/src/main.js
+++ rrpm-department/src/main.js
@@ -1,27 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createI18n } from "vue-i18n";
-import en from "./i18n/en.json";
-import ar from "./i18n/ar.json";
 import "./assets/styles/base.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
 
-// Set up translations
-const messages = {
-  en: en,
-  ar: ar
-};
 
-const i18n = createI18n({
-  locale: "en", // Default language
-  fallbackLocale: "en",
-  messages
-});
 
 const app = createApp(App);
 app.use(router);
-app.use(i18n);
 app.mount("#app");
