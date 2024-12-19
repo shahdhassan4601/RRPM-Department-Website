cb0e41c83fcf780ef97a172a98bacd63c178280a
shahd <shahd26@gmail.com>
2024-12-19 02:08:34 +0200

    making navbar responsive

--- rrpm-department/src/main.js
+++ rrpm-department/src/main.js
@@ -4,6 +4,10 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './assets/styles/base.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // Import Bootstrap JS (includes Popper.js)
+
 
 const app = createApp(App);
 app.use(router);
